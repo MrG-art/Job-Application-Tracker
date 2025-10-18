86084fad6abdab250d83d9b36188e95eaad67aa8
MrG-art <alidehi@hotmail.com>
2025-10-18 15:32:38 +0200

    Teständring för PR – JobManager

--- JobManager.cs
+++ JobManager.cs
@@ -1,4 +1,6 @@
-﻿// Jag importerar (using) biblioteket System för att komma åt klasser som Console och DateTime.
+// Teständring för Pull Request (Utveckling – JobManager)
+
+// Jag importerar (using) biblioteket System för att komma åt klasser som Console och DateTime.
 // Programmeringsord: using = importerar färdiga delar från C#-biblioteket.
 using System;
 
@@ -289,3 +291,4 @@ namespace JobTracker
         }
     }
 }
+
